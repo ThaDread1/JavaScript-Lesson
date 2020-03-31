6cc04334caac9c6fa8ef2b76e89299d742ee1866
Christopher Bell <32046267+ThaDread1@users.noreply.github.com>
2020-03-31 00:14:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(){
+'use strict'
+  	//My code will start here...
+  paper.install (window);
+  paper.setup (document.getElementById('mainCanvas'));
+  var c= Shape.Circle(200, 200, 50);
+  c.fillColor='green';
+  paper.view.draw ();
+}())
